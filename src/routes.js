31e48e66affbbe5e31f352d0fcbe21cf7037c9f6
laurie kemmerer <laurie.kemmerer@gmail.com>
2013-11-15 13:57:46 -0800

    Hook up stuff to use handlebars templates.

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 var forms = require('./routes/form');
 
 exports.setupHandlers = function setupHandlers(appServer) {
-  appServer.addRoute("/form", appServer.plugins.request, { section: "pre" });
-  appServer.addRoute("/form", forms.display);
+  appServer.addRoute("/form$", appServer.plugins.request, { section: "pre" });
+  appServer.addRoute("/form$", forms.display);
 
   appServer.addRoute(".+", appServer.plugins.filehandler, { basedir: "./public" });
 //  appServer.addRoute(".+", appServer.plugins.fourohfour);
