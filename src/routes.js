84dd0dfca8b92c3a1d84deb5f1befb42573bdb8c
laurie kemmerer <laurie.kemmerer@gmail.com>
2013-11-15 13:38:04 -0800

    add logging (and add logs directory to gitignore); other minor cleanup

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
-var forms = require('./routes/form')
+var forms = require('./routes/form');
 
 exports.setupHandlers = function setupHandlers(appServer) {
-  appServer.addRoute("/public/.+", appServer.plugins.filehandler, { basedir: "./public" });
+  appServer.addRoute("/form", appServer.plugins.request, { section: "pre" });
+  appServer.addRoute("/form", forms.display);
 
-  appServer.addRoute("^/form$", appServer.plugins.request, { section: "pre" });
-  appServer.addRoute("^/form$", forms.display);
+  appServer.addRoute(".+", appServer.plugins.filehandler, { basedir: "./public" });
+//  appServer.addRoute(".+", appServer.plugins.fourohfour);
 
-  appServer.addRoute(".+", appServer.plugins.fourohfour);
+  appServer.addRoute(".+", appServer.plugins.loghandler, { section: 'final', filename: "./logs/access.log" });
 
   return appServer;
 };
