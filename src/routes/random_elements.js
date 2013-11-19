4307f1607f8f66a42ed5cbcee75204ac8f49aa3d
laurie kemmerer <laurie.kemmerer@gmail.com>
2013-11-18 21:08:32 -0800

    Add suboptimal method of loading element partials and displaying the form.  Will add some grunt-powered precompilation soon.

--- src/routes/random_elements.js
+++ src/routes/random_elements.js
@@ -4,6 +4,9 @@ var randomizer = require('./../utilities/form_element_randomizer')
 exports.getElements = function getElements(request, response) {
   var randomElements = randomizer.randomizeElements(elements.allTheThings);
   var json = JSON.stringify(randomElements);
+
+  response.statusCode(200);
+  response.setHeader('Content-Type', 'application/json');
   response.write(json);
   response.end();
 };
