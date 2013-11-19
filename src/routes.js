9d1b69ade0b1d4aa48fa4b753b6aa4b24bf8a9e7
laurie kemmerer <laurie.kemmerer@gmail.com>
2013-11-18 21:51:35 -0800

    temporarily remove logging

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,5 @@ exports.setupHandlers = function setupHandlers(appServer) {
   appServer.addRoute(".+", appServer.plugins.filehandler, { basedir: "./public" });
 //  appServer.addRoute(".+", appServer.plugins.fourohfour);
 
-  appServer.addRoute(".+", appServer.plugins.loghandler, { section: 'final', filename: "./logs/access.log" });
-
   return appServer;
 };
