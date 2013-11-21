cb5f038e3780b0c63e14e2ca8639831411aa23e3
laurie kemmerer <laurie.kemmerer@gmail.com>
2013-11-20 21:05:07 -0800

    Precompile handlebars templates via grunt task.

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    handlebars: {
+      templates: {
+        files: {
+          'public/templates/templates.js': 'src/templates/*.handlebars',
+        },
+      },
+      partials: {
+        files: {
+          'public/templates/partials.js': 'src/templates/partials/*.partial'
+        },
+        options: {
+          namespace: 'Handlebars.partials',
+          partial: true
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-handlebars-compiler');
+  grunt.registerTask('compile-hbs', ['handlebars:templates', 'handlebars:partials']);
+};
